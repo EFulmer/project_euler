a35b30c22c9863b07c82b6e3b12530aba205a23c
Eric Fulmer <EFulmer@gmail.com>
2014-01-03 00:06:52 -0500

    trying to understand for in Clojure

--- problem-1.clj
+++ problem-1.clj
@@ -1,8 +1,21 @@
 
+(defn div-3-or-5 [n]
+  (or (= 0 (mod n 3)) (= 0 (mod n 5))))
+
 (defn problem-1-loop-recur [n]
   (loop [cur-sum 0
          i 1]
-    (cond (> i n) cur-sum
+    (cond (>= i n) cur-sum
           :else (cond 
-                  (or (= 0 (mod i 3)) (= 0 (mod i 5))) (recur (+ cur-sum i) (inc i))
+                  (div-3-or-5 i) (recur (+ cur-sum i) (inc i))
                   :else (recur cur-sum (inc i))))))
+
+
+(defn problem-1-for [n]
+  (reduce + (for [x (range 1 n)
+        :let [y x]
+        :when (div-3-or-5 y)]
+    y)))
+
+(defn problem-1-filter-reduce [n]
+  (reduce + (filter div-3-or-5 (range n))))
