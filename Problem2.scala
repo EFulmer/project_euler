b9af70f1d83805bca352b8ea1b23c17f6b6dca8e
Eric Fulmer <EFulmer@gmail.com>
2014-01-02 23:39:47 -0500

    a good Scala version of P2

--- Problem2.scala
+++ Problem2.scala
@@ -1,5 +1,5 @@
-// this is ugly
 object Problem2 extends Application {
+  // mutable:
   def fibsUpTo1(n: Int): List[Int] = {
     var fibLst = List(2, 1)
     while (fibLst(0) <= n) {
@@ -8,7 +8,18 @@ object Problem2 extends Application {
     return fibLst
   }
 
+  // tail-recursive accumulator function
+  def fibsUpTo2(n : Int) : List[Int] = {
+    def aux(fibsSoFar : List[Int]): List[Int] = {
+      if (fibsSoFar.head <= n)
+        aux( (fibsSoFar(0) + fibsSoFar(1)) :: fibsSoFar)
+      else
+        return fibsSoFar
+    }
+    aux(List(2, 1))
+  }
   def main(): Unit = {
     println(fibsUpTo1(4000000).filter(_ % 2 == 0).sum)
+    println(fibsUpTo2(4000000).filter(_ % 2 == 0).sum)
   }
 } 
