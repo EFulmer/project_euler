37331d32c0cbebad73ecd9a4c02e4635a77af11c
Eric Fulmer <EFulmer@users.noreply.github.com>
2016-11-05 18:33:02 -0400

    Delete problem_1.rb

--- problem_1.rb
+++
@@ -1,7 +0,0 @@
-# one-liner
-puts ((1..999).select { |n| n % 3 == 0 or n % 5 == 0 }).inject(0) { |x, y| x + y }
-
-# slightly cleaner
-mults = (1..999).select { |n| n % 3 == 0 or n % 5 == 0 }
-puts mults.inject(0) { |x, y| x + y }
-
